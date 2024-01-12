fcdba27304d383bb76d6040e9ec55c720cd09837
Gene Koval <genyaremchuk@protonmail.com>
2024-01-11 23:37:21 -0500

    Allow setting default config path during build

--- fstored/src/main.rs
+++ fstored/src/main.rs
@@ -1,11 +1,9 @@
 use fstored::conf;
 
 use clap::Parser;
-use std::{
-    path::{Path, PathBuf},
-    process::ExitCode,
-};
+use std::{env, path::PathBuf, process::ExitCode};
 
+const COMPILE_CONFIG: Option<&str> = option_env!("FSTORED_DEFAULT_CONFIG");
 const DEFAULT_CONFIG: &str = "/etc/fstore/fstore.yml";
 
 #[derive(Parser)]
@@ -18,12 +16,18 @@ pub struct Cli {
 fn main() -> ExitCode {
     let cli = Cli::parse();
 
-    let config = match cli.config.as_deref() {
-        Some(config) => config,
-        None => Path::new(DEFAULT_CONFIG),
+    let config = match cli.config {
+        Some(ref config) => config.clone(),
+        None => match env::var_os("FSTORED_CONFIG") {
+            Some(config) => PathBuf::from(&config),
+            None => match COMPILE_CONFIG {
+                Some(config) => PathBuf::from(config),
+                None => PathBuf::from(DEFAULT_CONFIG),
+            },
+        },
     };
 
-    let config = match conf::read(config) {
+    let config = match conf::read(&config) {
         Ok(config) => config,
         Err(err) => {
             eprintln!("{err}");
